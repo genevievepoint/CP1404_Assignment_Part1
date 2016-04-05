ffe57d50aa4dcc491662983534a75e9282d026d4
genevieve.point <genevieve.point@my.jcu.edu.au>
2016-04-06 01:50:36 +1000

    Third time lucky, changed back to original cvs open format as it appears neater and more concise. Have managed to get it to format correctly as it is in the sample output. Need to add in the menu and other functions now. Hopefully it isn't as bad

--- code_part1.py
+++ code_part1.py
@@ -2,22 +2,23 @@
 import csv
 
 # menu()
-print("Items for Hire - By Genevieve Point")# 06/04/2016
+# print("Items for Hire - By Genevieve Point")# 06/04/2016
 
 
-def load_list():
-    workbook_file = open('items.csv', 'r')
-    workbook_reader = csv.reader(workbook_file)
+workbook_file = open('items.csv', 'r')
+workbook_reader = csv.reader(workbook_file)
 
-    items_list = []
+sheets_list = []
 
-    for row in workbook_reader:
-        # row = row.split()
-        #  row_info = row.strip('\n')
-        items_list.append(row)
-        items_list = row
-        # print(items_list)
+for row in workbook_reader:
+    print(row)
 
+    # row = row.split()
+    # row_info = row.strip('\n')
+    sheets_list.append(row)
+    items_list = row
+    print(items_list)
+    #
     counter_strip = 0
     for lists in items_list:
         items_list[counter_strip] = items_list[counter_strip].strip()
@@ -34,52 +35,53 @@ def load_list():
         working_list = items_list[counter_main]
         counter_main += 1
 
-        # print("() -{} [()]\t\t\t= $()",format(counter_main, working_list[0], " ", working_list[2], working_list[3])
-        print(working_list)
+    print("() -{} [()]\t\t\t= $()",format(counter_main, working_list[0], " ", working_list[2], working_list[3])
+    # print(working_list)
 
     workbook_reader.close()
 
 
-def menu():
-    print("Menu: \n (L)ist all items \n (H)ire an item \n (R)eturn an item \n (A)dd an item \n (Q)uit \n")
-    choice = input()
+    # print(sheets_list)
+# def menu():
+#     print("Menu: \n (L)ist all items \n (H)ire an item \n (R)eturn an item \n (A)dd an item \n (Q)uit \n")
+#     choice = input()
+#
+#     if choice == "L" or choice == "l":
+#         list_items()
+#         menu()
+#     elif choice == "H" or choice == "h":
+#         hire_item()
+#         menu()
+#     elif choice == "R" or choice == "r":
+#         return_item()
+#         menu()
+#     elif choice == "A" or choice == "a":
+#         add_item()
+#         menu()
+#     elif choice == "Q" or choice == "q":
+#         print("quit()")
+#     else:
+#         print("Please enter a valid choice")
+#         menu()
+#
 
-    if choice == "L" or choice == "l":
-        list_items()
-        menu()
-    elif choice == "H" or choice == "h":
-        hire_item()
-        menu()
-    elif choice == "R" or choice == "r":
-        return_item()
-        menu()
-    elif choice == "A" or choice == "a":
-        add_item()
-        menu()
-    elif choice == "Q" or choice == "q":
-        print("quit()")
-    else:
-        print("Please enter a valid choice")
-        menu()
 
-
-
-    def load_items():
-        print("List all items")
+    # def load_items():
+    #     print("List all items")
 
 
 
 
 # menu()
 
-def list_items(items_list):
-    print("All items on file")
+# def list_items(items_list):
+#     print("All items on file")
     # if items_list == "out":
     #     print("*")
 
 
-def hire_item():
-    print("Hire an item")
+# def hire_item():
+#     print("Hire an item")
 #     for line in lines:
 #     if country_name in line:
 #         line = line.strip().split(',')
