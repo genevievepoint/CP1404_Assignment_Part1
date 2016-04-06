be49b55509480af0f441e6594389318c5c2a2efb
genevieve.point <genevieve.point@my.jcu.edu.au>
2016-04-06 12:07:47 +1000

    Started on other function for hire, adding an item and such. Only problem i having trouble calling them in the menu and getting them to work properly. Will have to look into this further.

--- code_part1.py
+++ code_part1.py
@@ -1,23 +1,23 @@
 # Genevieve Point
 
 import csv
-
-
+# Opens the file in a table format
 workbook_file = open('items.csv', 'r')
-# workbook_reader = csv.reader(workbook_file)
 workbook_list = workbook_file.readlines()
 
+# Separates the rows from each other
 counter = 0
 for items in workbook_list:
     workbook_list[counter] = items.strip()
     counter += 1
 
+# Separates the items in each row from each other
 counter2 = 0
 for items in workbook_list:
     workbook_list[counter2] = items.split(',')
     counter2 += 1
 
-
+# Formats the prices to sit in the same position in each row
 counter_main = 0
 blank_space = ''
 for i in workbook_list:
@@ -25,7 +25,7 @@ for i in workbook_list:
     spacing_length = 38 - (len(working_list[0] + working_list[1]))
 
     for i in range(1, spacing_length, 1):
-            blank_space += ' '
+        blank_space += ' '
             # counter += 1
     # print(working_list)
 
@@ -37,17 +37,11 @@ for i in workbook_list:
         print("{} - {} ({}){}\t\t\t= ${}\t{}".format(counter_main, working_list[0], working_list[1], blank_space, working_list[2],"*"))
         counter_main += 1
         blank_space = ' '
-    #
+
     # print("{} - {} [{}]{}\t\t\t= ${}\t{}".format(counter_main, working_list[0], working_list[1], ' ', working_list[2], working_list[3]))
     # counter_main += 1
     # blank_string = ' '
 
-# print(workbook_list)
-
-
-# print(workbook_list)
-
-workbook_file.close()
 
 
 def menu():
@@ -55,7 +49,7 @@ def menu():
     choice = input()
 
     if choice == "L" or choice == "l":
-        print("Load item")
+        list_all_items()
         menu()
     elif choice == "H" or choice == "h":
         print("Hire item")
@@ -64,10 +58,100 @@ def menu():
         print("Return an item")
         menu()
     elif choice == "A" or choice == "a":
-        print("Add an item")
+        add_item()
         menu()
     elif choice == "Q" or choice == "q":
         quit()
     else:
         print("Please enter a valid choice")
         menu()
+
+
+menu()
+
+
+def list_all_items():
+
+    workbook_file = open('items.csv', 'r')
+    # workbook_reader = csv.reader(workbook_file)
+    workbook_list = workbook_file.readlines()
+
+    counter = 0
+    for items in workbook_list:
+        workbook_list[counter] = items.strip()
+        counter += 1
+
+    counter2 = 0
+    for items in workbook_list:
+        workbook_list[counter2] = items.split(',')
+        counter2 += 1
+
+
+    counter_main = 0
+    blank_space = ''
+    for i in workbook_list:
+        working_list = workbook_list[counter_main]
+        spacing_length = 38 - (len(working_list[0] + working_list[1]))
+
+        for i in range(1, spacing_length, 1):
+            blank_space += ' '
+            # counter += 1
+            # print(working_list)
+
+
+        if working_list[3] == 'in':
+            return("{} - {} ({}){}\t\t\t= ${}\t{}".format(counter_main, working_list[0], working_list[1], blank_space, working_list[2], " "))
+            # counter_main += 1
+            # blank_space = ' '
+        elif working_list[3] == 'out':
+            return("{} - {} ({}){}\t\t\t= ${}\t{}".format(counter_main, working_list[0], working_list[1], blank_space, working_list[2],"*"))
+            # counter_main += 1
+            # blank_space = ' '
+    # print("{} - {} [{}]{}\t\t\t= ${}\t{}".format(counter_main, working_list[0], working_list[1], ' ', working_list[2], working_list[3]))
+    # counter_main += 1
+    # blank_string = ' '
+
+    workbook_file.close()
+
+    # list_items()
+
+
+def hire_item(working_list, blank_space, counter_main):
+
+    working_list[3] == 'in'
+    print("{} - {} ({}){}\t\t\t= ${}\t{}".format(counter_main, working_list[0], working_list[1], blank_space, working_list[2], " "))
+    counter_main += 1
+    blank_space = ' '
+
+
+def add_item():
+    for row in workbook_list:
+        workbook_list.append(row)
+
+        workbook_list[0] = input("Item name: ")
+        workbook_list[1] = input("Description: ")
+        workbook_list[2] = input("Price per day: ")
+
+
+
+
+
+
+workbook_file.close()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
